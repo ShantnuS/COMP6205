f731743baab8021e9dd4fffd6c66ff48a5067708
ShantnuS <shantnu@outlook.com>
2018-11-16 17:22:07 +0000

    basic logging in

--- HelloHomes/HelloHomes/Pages/Account/login.cshtml.cs
+++ HelloHomes/HelloHomes/Pages/Account/login.cshtml.cs
@@ -1,16 +1,57 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HelloHomes.Pages.Account
 {
-    public class loginModel : PageModel
+    public class LoginModel : PageModel
     {
-        public void OnGet()
+        [BindProperty]
+        [Required]
+        [Display(Name = "Email")]
+        public string EmailAddress { get; set; }
+
+        [BindProperty]
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public IActionResult OnPost()
         {
+            var isValidUser = EmailAddress == "admin" && Password == "password";
+
+            if (!isValidUser)
+            {
+                ModelState.AddModelError("", "Invalid email or password");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var scheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            var user = new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new[] { new Claim(ClaimTypes.Name, EmailAddress) },
+                    scheme
+                )
+                );
+
+            return SignIn(user, scheme);
+        }
+
+        public async Task<IActionResult> OnPostLogoutAsync()
+        {
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToPage("/Index");
         }
     }
 }
