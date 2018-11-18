cbbd315e83e5d0971e4f59566bd0eda837e47ca0
ShantnuS <shantnu@outlook.com>
2018-11-18 12:35:18 +0000

    property model improvements

--- HelloHomes/HelloHomes/Models/Property.cs
+++ HelloHomes/HelloHomes/Models/Property.cs
@@ -14,10 +14,33 @@ namespace HelloHomes.Models
         [Required]
         [StringLength(100, MinimumLength = 5,
             ErrorMessage = "Hey - you've gotta give a name between 5 and 100 characters long!")]
+        //Name of the property (i.e. "1 Baker Street")
         public string Name { get; set; }
 
         [Required]
+        //Description of Property
         public string Description { get; set; }
 
+        //Image of the property
+        public byte[] Image { get; set; }
+
+        //Number of bedrooms in the property
+        public int Bedrooms { get; set; }
+
+        //Cost of property per month
+        public decimal RentPerMonth { get; set; }
+
+        //Name of landlord
+        public string LandlordName { get; set; }
+
+        //Phone number of landlord
+        public string LandlordNumber { get; set; }
+
+        /* Approval status of property
+         * 0 = Pending
+         * 1 = Approved 
+         * 2 = Rejected
+         */
+        public int ApprovalStatus { get; set; }
     }
 }
