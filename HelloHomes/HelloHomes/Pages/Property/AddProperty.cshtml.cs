b0fd8623441f52d9dea75bebb2f47f38c53eea74
P1N6 <dm3g14@soton.ac.uk>
2018-11-26 22:04:34 +0000

    allow adding properties

---
+++ HelloHomes/HelloHomes/Pages/Property/AddProperty.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloHomes.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HelloHomes.Pages.Property
+{
+    public class AddPropertyModel : PageModel
+    {
+        [BindProperty]
+        [Required]
+        [Display(Name = "Property Name")]
+        public string Name { get; set; }
+
+        [BindProperty]
+        [Required]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [BindProperty]
+        [Required]
+        [Display(Name = "No. of Bedrooms")]
+        public int Bedrooms { get; set; }
+
+        [BindProperty]
+        [Required]
+        [Display(Name = "Rent (£ pcm)")]
+        public decimal RentPerMonth { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var personService = new PersonService();
+            var propertyService = new PropertyService();
+
+            var person = await personService.FindByEmailAsync(User.Identity.Name);
+
+            //Place to put data into
+            Models.Property property = new Models.Property
+            {
+                Name = Name,
+                Description = Description,
+                Bedrooms = Bedrooms,
+                RentPerMonth = RentPerMonth,
+                LandlordID = person.Id,
+                LandlordName = person.FullName,
+                LandlordNumber = person.PhoneNumber,
+                ApprovalStatus = Models.Property.ApprovalEnum.Pending,
+                ApprovalComment = ""
+            };
+
+            await propertyService.SaveAsync(property);
+            return RedirectToPage("/List");
+        }
+    }
+}
